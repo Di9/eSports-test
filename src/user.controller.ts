4e0caa66ff905440054639bb55012ba6e71b3652
Dmitriy K <dmitriy.kikinskiy@pay-me.ru>
2019-03-06 18:57:55 +0300

    simplest logic and bacis methods

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body} from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getHello(): object {
+  allUsers(): object {
     return this.userService.findAllUsers();
   }
+
+  @Get(':id')
+  userData(@Param('id') id): object {
+      return this.userService.findUserById(id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    this.userService.createUser(createUserDto);
+    return 'New user was created';
+  }
+
 }
--- src/user.service.ts
+++ src/user.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
+
+  public users = [
+      {userLogin: 'Vasya'},
+      {userLogin: 'Petya'},
+  ];
+
   findAllUsers(): object {
-    return ['Vasya', 'Petya'];
+    return this.users;
   }
+
+  findUserById(id): object {
+    return this.users[id];
+  }
+
+  createUser(user): string {
+    this.users.push(user);
+    return 'ok';
+  }
+
 }
