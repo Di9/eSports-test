a4a60d6b0319b29e906643137ffed1127a9ebbdf
Dmitriy K <dmitriy.kikinskiy@pay-me.ru>
2019-03-06 18:15:06 +0300

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { UserModule } from './user.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(UserModule);
+
+  const options = new DocumentBuilder()
+      .setTitle('eSports test example')
+      .setDescription('User data API')
+      .setVersion('0.0.1')
+      .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
